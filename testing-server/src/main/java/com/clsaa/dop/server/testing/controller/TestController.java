5ec55724db53b80744741afa8340942a09d5eeaa
Vettel1998 <1105612690@qq.com>
2020-03-06 14:26:30 +0800

    add two controllers

--- testing-server/src/main/java/com/clsaa/dop/server/testing/controller/TestController.java
+++ testing-server/src/main/java/com/clsaa/dop/server/testing/controller/TestController.java
@@ -4,12 +4,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class testController {
+public class TestController {
     @GetMapping("/test")
     public String test(){
         return "test";
     }
-
 
 }
 
