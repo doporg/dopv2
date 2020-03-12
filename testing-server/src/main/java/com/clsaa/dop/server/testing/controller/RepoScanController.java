eab50feaf91d4883ed6f5e7cdb3398ce43ae5653
Vettel1998 <1105612690@qq.com>
2020-03-13 01:56:32 +0800

    fix error in pom.xml and test in linux

--- testing-server/src/main/java/com/clsaa/dop/server/testing/controller/RepoScanController.java
+++ testing-server/src/main/java/com/clsaa/dop/server/testing/controller/RepoScanController.java
@@ -1,24 +1,27 @@
 package com.clsaa.dop.server.testing.controller;
 
 
-import com.sun.javaws.Main;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.io.FileUtils;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
 import org.sonarsource.scanner.api.EmbeddedScanner;
-import org.springframework.util.ResourceUtils;
+import org.sonarsource.scanner.api.ScanProperties;
+import org.sonarsource.scanner.api.StdOutLogOutput;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
  * 代码扫描接口
  * @author Vettel
  *
+ *
+ * 
  */
 
 @CrossOrigin
@@ -50,7 +53,23 @@ public class RepoScanController {
 
         // clean up here to not keep using more and more disk-space for these samples
         //FileUtils.deleteDirectory(localPath);
+        Map<String, String> sonarPropertiesMap = new LinkedHashMap<String, String>();
+        sonarPropertiesMap.put("sonar.host.url", "http://192.168.31.151:9000");
+        sonarPropertiesMap.put("sonar.sourceEncoding", "UTF-8");
+        sonarPropertiesMap.put("sonar.login", "4e8fccc6f61e49e681e91526ecac77b16afebb95");
 
+        Map<String, String> projectSettingMap = new LinkedHashMap<>();
+        projectSettingMap.put(ScanProperties.PROJECT_KEY, "test_project_01");
+        projectSettingMap.put(ScanProperties.PROJECT_BASEDIR, localPath.getAbsolutePath());
+        projectSettingMap.put(ScanProperties.PROJECT_SOURCE_DIRS, "src/main/java");
+        projectSettingMap.put(ScanProperties.PROJECT_SOURCE_ENCODING, "UTF-8");
+        projectSettingMap. put("sonar.java.binaries", "target/classes");
+        projectSettingMap.put("sonar.java.source", "src/main/java");
+
+        EmbeddedScanner scanner = EmbeddedScanner.create("Maven", "3.6.1", new StdOutLogOutput());
+        scanner.addGlobalProperties(sonarPropertiesMap);
+        scanner.start();
+        scanner.execute(projectSettingMap);
 
 
     }
